abea230a2b0b5cb8b20a7aabcb58a430fe520a56
gabrielbaute <gabrielbaute@gmail.com>
2025-12-11 09:19:38 -0400

    Pruebas

---
+++ tests/ffmpg_splitter.py
@@ -0,0 +1,14 @@
+import logging
+from app.services import TrackListParser, FFmpegSplitter
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+track_list_file = "tracklist.txt"
+audio_file = "1990_Stellar_Circuit_Fleet.mp3"
+
+parser = TrackListParser(track_list_file)
+tracklist = parser.parse()
+
+splitter = FFmpegSplitter(audio_file)
+splitter.split_all(tracklist)
---
+++ tests/track_list_parser.py
@@ -0,0 +1,14 @@
+import logging
+from app.services import TrackListParser
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file = "tracklist.txt"
+parser = TrackListParser(file)
+tracklist = parser.parse()
+
+print(f"Total time: {tracklist.total_time}")
+print(f"Total tracks: {tracklist.total_tracks}")
+
+for track in tracklist.tracks:
+    print(f"Start: {track.start}, End: {track.end}, Title: {track.title}")
