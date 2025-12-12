f18b04864cab0e41855ab5210e7cac89a83dc9b7
gabrielbaute <gabrielbaute@gmail.com>
2025-12-12 14:37:53 -0400

    Tests completos del core

---
+++ tests/all_process.py
@@ -0,0 +1,23 @@
+import logging
+from app.services import TrackListParser, FFmpegSplitter, MutagenTagger
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+track_list_file = "tracklist.txt"
+audio_file = "1990_Stellar_Circuit_Fleet.mp3"
+
+parser = TrackListParser(track_list_file)
+tracklist = parser.parse()
+
+splitter = FFmpegSplitter(audio_file)
+files = splitter.split_all(tracklist)
+
+tagger = MutagenTagger(
+    album="1990 Stellar Circuit Fleet",
+    artist="Zyykron Sound",
+    year=2025,
+    genre="Synthwave",
+    cover="cover.jpeg"
+)
+tagger.tag_all(files, tracklist)
